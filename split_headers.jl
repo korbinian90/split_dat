6892c67b2b219ccc70d100a6089753fc81c642e7
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-12-25 05:32:50 +0100

    works with multiple scans

--- split_headers.jl
+++ split_headers.jl
@@ -1,141 +1,85 @@
-function split_dat_water(filename, f_out; n_channels=32)
+function split_dat_metabolite(filename, f_out; n_channels, metabolite)
+    println("writing metabolite:")
+    select = scan -> scan.dims[SET] in metabolite
+    split_dat(filename, f_out; n_channels, select)
+end
+
+function split_dat_water(filename, f_out; n_channels)
+    println("writing water:")
+    select = scan -> scan.ice_param[6] == 1
+    split_dat(filename, f_out; n_channels, select)
+end
+
+function split_dat(filename, f_out; n_channels, select)
     out = open(f_out, "w")
     open(filename) do io
-        seek_to_first_scan_header!(io)
-        pos = position(io)
-        # seek io to beginning of file
-        seek(io, 0)
-        write(out, read(io, pos))
-
         set_offset = 40 + 8 + 9 * 2
         i_water = 0
 
-        while true
-            pos_begin = position(io)
-            scan = read(io, ScanHeaderVD)
+        file_header = read(io, HeaderInfo)
+        offset = file_header.meas_offset[1]
+        seek(io, offset)
 
-            if scan.type == :ACQEND
-                seek(io, pos_begin)
-                write(out, read(io))
-                break
-            end
-
-            seek_to_next_header!(io, scan.data_position, n_channels)
-            pos_next = position(io)
-            nbytes = pos_next - pos_begin
-
-            # Decide which scan goes to which file
-            seek(io, pos_begin)
-            bytes = read(io, nbytes)
-            if scan.ice_param[6] == 1 # check if it is water
-                bytes[set_offset+1] = i_water
-                i_water += 1
-                write(out, bytes)
-            else
-                seek(io, pos_next) # skip others
-            end
-        end
-    end
-    close(out)
-end
-
-function split_dat_metabolite(filename, f_out; n_channels=32, metabolite)
-    out = open(f_out, "w")
-    open(filename) do io
-        seek_to_first_scan_header!(io)
         pos = position(io)
         # seek io to beginning of file
         seek(io, 0)
         write(out, read(io, pos))
+        seek(io, offset)
 
-        set_offset = 40 + 8 + 9 * 2
-        i_scan = 0
-
-        while true
+        for i_scan = 1:file_header.n_scans
             pos_begin = position(io)
-            scan = read(io, ScanHeaderVD)
-
-            if scan.type == :ACQEND
-                seek(io, pos_begin)
-                write(out, read(io))
-                break
-            end
-
-            seek_to_next_header!(io, scan.data_position, n_channels)
-            pos_next = position(io)
-            nbytes = pos_next - pos_begin
-
-            # Decide which scan goes to which file
+            skip_twix(io)
+            pos = position(io)
+            # seek io to beginning of file
             seek(io, pos_begin)
-            bytes = read(io, nbytes)
-            if scan.dims[SET] in metabolite
-                bytes[set_offset+1] = i_scan # Works for up to 255 sets, then the first byte needs to be addressed
-                i_scan += 1
-                write(out, bytes)
-            else
-                seek(io, pos_next)
+            write(out, read(io, pos - pos_begin))
+
+            while true
+                pos_begin = position(io)
+                scan = read(io, ScanHeaderVD)
+
+                if scan.type == :ACQEND
+                    seek(io, pos_begin)
+                    write(out, read(io, 352))
+                    pos = position(io)
+                    if mod(pos, 512) > 0
+                        pos = pos + 512 - mod(pos, 512)
+                    end
+                    seek(io, pos)
+                    break
+                end
+
+                if scan.type == :SYNCDATA
+                    len = 1728
+                    seek(io, pos_begin)
+                    write(out, read(io, len))
+                    continue
+                end
+
+                seek_to_next_header!(io, scan.data_position, n_channels)
+                pos_next = position(io)
+                nbytes = pos_next - pos_begin
+
+                # Decide which scan goes to which file
+                if select(scan)
+                    println(scan.dims)
+                    seek(io, pos_begin)
+                    bytes = read(io, nbytes)
+                    bytes[set_offset+1] = i_water
+                    i_water += 1
+                    write(out, bytes)
+                else
+                    seek(io, pos_next) # skip others
+                end
             end
         end
     end
     close(out)
 end
 
-function split_scan_headers(filename, f1, f2, n_channels)
-    o1 = open(f1, "w")
-    o2 = open(f2, "w")
-    open(filename) do io
-        seek_to_first_scan_header!(io)
-        pos = position(io)
-        # seek io to beginning of file
-        for o in (o1, o2)
-            seek(io, 0)
-            write(o, read(io, pos))
-        end
-
-        set_offset = 40 + 8 + 9 * 2
-        i_water = i_scan = 0
-
-        while true
-            pos_begin = position(io)
-            scan = read(io, ScanHeaderVD)
-
-            if scan.type == :ACQEND
-                for o in (o1, o2)
-                    seek(io, pos_begin)
-                    write(o, read(io))
-                end
-                break
-            end
-
-            seek_to_next_header!(io, scan.data_position, n_channels)
-            pos_next = position(io)
-            nbytes = pos_next - pos_begin
-
-            # Decide which scan goes to which file
-            seek(io, pos_begin)
-            bytes = read(io, nbytes)
-            if scan.ice_param[6] == 0 && scan.dims[SET] == 1
-                bytes[set_offset+1] = i_water
-                i_water += 1
-                write(o1, bytes)
-            elseif 3 ≤ scan.dims[SET] ≤ 18
-                bytes[set_offset+1] = i_scan # Works for up to 255 sets, then the first byte needs to be addressed
-                i_scan += 1
-                write(o2, bytes)
-            else
-                seek(io, pos_next)
-            end
-        end
-    end
-end
-
-function seek_to_first_scan_header!(io; scan=1)
-    file_header = read(io, HeaderInfo)
-    offset = file_header.meas_offset[scan]
-    seek(io, offset)
-    # skip twix header
+function skip_twix(io)
     header_length = read(io, UInt32)
-    seek(io, offset + header_length)
+    seek(io, position(io) + header_length - 4)
 end
 
 function seek_to_next_header!(io, (start, n_adc), n_channels)
@@ -157,6 +101,8 @@ end
 function scan_info(mask)
     if mask[1]
         return :ACQEND
+    elseif mask[6]
+        return :SYNCDATA
     elseif mask[26]
         return :NOISADJSCAN
     elseif mask[24]
